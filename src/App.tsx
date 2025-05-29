63bb78f3e7baf197362054290865e8319402d111
Juan Sebastian Diaz <1116070867@u.icesi.edu.co>
2025-05-28 22:08:05 -0500

    Actualiza .gitignore y README.md: se añaden nuevas reglas de ignorado y se mejora la documentación sobre la configuración de React con TypeScript y Vite.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-600">¡Tailwind está funcionando!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
